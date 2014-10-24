dc7a6bcef9411d4a2944c2093fb1c2121bcffe8e
jplarar <jplarar@gmail.com>
2014-10-24 01:48:45 -0500

    Tabbed Application template

---
+++ RELEASE 1/RELEASE 1/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  RELEASE 1
+//
+//  Created by Juan Paulo Lara on 10/24/14.
+//  Copyright (c) 2014 JuanPauloLara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
