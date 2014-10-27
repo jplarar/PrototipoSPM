4d4c8629c5bcf412fe590d2e937c7a5ed3a66c52
jplarar <jplarar@gmail.com>
2014-10-27 00:27:47 -0600

    Fix documentation

--- RELEASE 1/RELEASE 1/MCManager.h
+++ RELEASE 1/RELEASE 1/MCManager.h
@@ -1,29 +1,47 @@
 //
 //  MCManager.h
+//  Prototipo para simulador de procesos de manufactura
+//  Juan Paulo Lara, Manuel Calzado y Andrés López De León
+//
 //  RELEASE 1
 //
-//  Created by Juan Paulo Lara on 10/24/14.
-//  Copyright (c) 2014 JuanPauloLara. All rights reserved.
+//  Fecha de creación: 10/24/14
+//  Fecha de última actualización: 10/27/14
+//  Descripción general: Archivo donde se almacenan las variables a utilizar para
+//  el framework de multipeer connectivity.
 //
-//This file is part of "Prototipo para simulador de procesos de manufactura".
+//  Copyright (c) 2014 ITESM. All rights reserved.
 //
-//"Prototipo para simulador de procesos de manufactura" is free software:
-//you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+//  This file is part of "Prototipo para simulador de procesos de manufactura".
 //
-//"Prototipo para simulador de procesos de manufactura" is distributed in
-//the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+//  "Prototipo para simulador de procesos de manufactura" is free software:
+//  you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
-//You should have received a copy of the GNU General Public License
-//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//  "Prototipo para simulador de procesos de manufactura" is distributed in
+//  the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 //
 // Code retrived from Gabriel Theodoropoulos tutorial
 // <http://www.appcoda.com/intro-multipeer-connectivity-framework-ios-programming/>
+//
+//  Authors:
+//
+//  ITESM representatives
+//  Ing. Martha Sordia Salinas  <msordia@itesm.mx>
+//  Ing. Mario de la Fuente     <mario.delafuente@itesm.mx>
+//
+//  ITESM students
+//  Juan Paulo Lara Rodríguez   <jplarar@gmail.com>
+//  Manuel Calzado              <mcm_maycod@hotmail.com>
+//  Andrés López De León        <agldeleon@gmail.com>
 //
 
 #import <Foundation/Foundation.h>
